7be44df4f124c0cf4dc70807d1e14c36e6072a1b
John Bouchard <jbouchard13@gmail.com>
2020-05-16 21:14:29 -0400

    grabbed current day values from weather api

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 // weather api key
 var apiKey = "138ccdcbc1c05e83db93a0593cb96881";
 
+var weatherDiv = $("#weatherDiv");
+
 // add an event listener to the submit button
 $("#submit").on("click", function (e) {
   e.preventDefault();
@@ -8,12 +10,34 @@ $("#submit").on("click", function (e) {
   var cityInput = $("#cityInput").val().trim();
   // var stateInput = $("#stateInput").val().trim();
 
-  console.log(cityInput, stateInput);
+  // add the is-loading class to the input to show the loading symbol
+  $(".control").addClass("is-loading");
+  console.log(cityInput);
 
   // create the api call source for current weather conditions
   var currentSrc = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
   // start the ajax call to the openweather api for current weather conditions
   $.get(currentSrc).then(function (response) {
     console.log(response);
+    // remove the loading symbol from the input box
+    $(".control").removeClass("is-loading");
+
+    // get the current temperature, humidity, wind speed, and icon ID
+    var currentKelvin = response.main.temp;
+    // convert the temperature in kelvin to fahrenheit
+    var currentTemp = Math.round((currentKelvin - 273.15) * 1.8 + 32);
+    var currentHumidity = response.main.humidity;
+    var currentWind = response.wind.speed;
+    var currentID = response.weather[0].id;
+    console.log(
+      "temp",
+      currentTemp,
+      "humidity",
+      currentHumidity,
+      "wind",
+      currentWind,
+      "iconID",
+      currentID
+    );
   });
 });
